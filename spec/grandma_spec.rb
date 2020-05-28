e6389eebc30d77e10f1ef932ba37eb966e9c7c95
Fatoumata A Diaby <fatimad5293@gmail.com>
2020-05-28 17:28:27 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -1,20 +1,3 @@
-if speak_to_grandma == I LOVE YOU GRANDMA!
-  puts "I LOVE YOU TOO PUMPKIN!"
-end
 
-if speak_to_grandma == Hi Nana, how are you?
-  puts "HUH?! SPEAK UP, SONNY!"
-end
-
-if speak_to_grandma == HI!
-  puts "HUH!? SPEAK UP, SONNY!"
-  
-if speak_to_grandma == WHAT DID YOU EAT TODDAY?!
-  puts "NO, NOT SINCE 1938!"
-end
-
-if speak_to_grandma== WHAT?
-  puts "NO, NOT SINCE 1938!"
-end 
 
 
